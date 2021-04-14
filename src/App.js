6d20aca8b39a79ba137b2f83125a2dec5ce2ae7b
Sumit Upadhyay <sumit.kumar.upadhyay@gmail.com>
2021-04-14 20:50:12 +0530

    react, router, json-server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Select from './components/Select';
+import NotFound from './components/NotFound';
+import Create from './components/Create';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+            <Route exact path="/">
+                <Select />
+            </Route>
+            <Route  path="/Create">
+              <Create />
+          </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
